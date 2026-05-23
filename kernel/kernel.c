8f55bd3899188dd7b881dcb3d6b56208688a8387
cybertarr-A <jashrajs.valab@gmail.com>
2026-05-24 03:13:17 +0530

    V1E0 the NEXT

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,23 +1,39 @@
 #include <stdint.h>
 
-volatile uint16_t* video_memory=(uint16_t*)0xB8000;
+#define MULTIBOOT_MAGIC 0x1BADB002
+#define MULTIBOOT_FLAGS 0
+#define MULTIBOOT_CHECKSUM \
+-(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
 
-void print(const char* str)
+__attribute__((section(".multiboot")))
+const unsigned long multiboot_header[] =
+{
+    MULTIBOOT_MAGIC,
+    MULTIBOOT_FLAGS,
+    MULTIBOOT_CHECKSUM
+};
+
+volatile uint16_t* vga =
+(uint16_t*)0xB8000;
+
+void print(const char* s)
 {
     int i=0;
 
-    while(str[i]!=0)
+    while(s[i])
     {
-        video_memory[i]=(0x0F<<8)|str[i];
+        vga[i] =
+        (0x0F<<8)|s[i];
+
         i++;
     }
 }
 
-void kernel_main()
+void kernel_main(void)
 {
     print("AK-1 AI Kernel Booted");
 
-    while(1)
+    for(;;)
     {
         __asm__("hlt");
     }
